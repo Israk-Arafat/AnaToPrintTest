031b404bbc07968133e0aeb08f1f47c79c9b287a
Israk <israkayasin@gmail.com>
2026-02-23 19:41:41 -0500

    removed unused package

--- webapp/src/hooks/__tests__/useDicomUpload.test.ts
+++ webapp/src/hooks/__tests__/useDicomUpload.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
-import { renderHook, act, waitFor } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useDicomUpload } from "../useDicomUpload";
 import type { DicomFileInfo } from "../../utils/dicomUtils";
 
