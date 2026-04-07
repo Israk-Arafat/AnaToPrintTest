45206fb92dada261fe5425a8f53e46b4e84e8b2b
Israk <israkayasin@gmail.com>
2026-04-07 01:21:49 -0400

    further testing for hosting

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -5,6 +5,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+console.log("[AnaToPrint] App starting");
+console.log("[AnaToPrint] Base URL:", import.meta.env.BASE_URL);
+console.log("[AnaToPrint] Mode:", import.meta.env.MODE);
+console.log("[AnaToPrint] Location:", window.location.href);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
